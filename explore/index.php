372d88760d9640d7032b7a2727b9e79a196c5b41
Steven Yuen <steven@mother.co>
2016-12-05 19:44:02 -0600

    Initial style commit

---
+++ explore/index.php
@@ -0,0 +1,54 @@
+<?
+   $headerStyle = 'welcome';
+   include '../header.php'
+?>
+<div class="explore-header">
+   <div class="headline">
+      <div class="welcome-icon icon ion-ios-book-outline"></div>
+      <div class="title">Explore Documentation</div>
+      <div class="subtitle">The official guide and reference for GNS3</div>
+      <div class="search-input">
+         <div class="icon ion-android-search"></div>
+         <input type="text" placeholder="Search Documentation">
+      </div>
+   </div>
+</div>
+<div class="wrapper">
+   <div class="explore-block">
+      <div class="icon ion-ios-gear-outline"></div>
+      <h1>Installation</h1>
+      <a href="/article">Installation on Windows with a local server</a>
+      <a href="/article">Installation on Windows with the GNS3 VM</a>
+      <a href="/article">Installation on Mac OS X</a>
+      <a href="/article">Installation on Linux</a>
+      <a href="/article">Install on a remote server</a>
+   </div>
+   <div class="explore-block">
+      <div class="icon ion-ios-bolt-outline"></div>
+      <h1>Using GNS3</h1>
+      <h2>Beginner</h2>
+      <a href="#">Your first GNS3 topology</a>
+      <a href="#">Your first Cisco GNS3 Topology</a>
+      <a href="#">The GNS3 GUI</a>
+      <a href="#">Change node symbol</a>
+      <a href="#">GNS3 Styles</a>
+      <a href="#">Connect GNS3 to the internet</a>
+      <a href="#">Switching and GNS3</a>
+      <div class="section-break"></div>
+      <h2>Advanced</h2>
+      <a href="#">GNS3 server configuration file</a>
+      <a href="#">Running the GNS3 server as a daemon</a>
+      <a href="#">Client server SSL encryption</a>
+      <a href="#">Special IPs in GNS3</a>
+   </div>
+   <div class="explore-block">
+      <div class="icon ion-ios-analytics-outline"></div>
+      <h1>Emulators</h1>
+      <a href="#">Which emulator shoud I use?</a>
+      <a href="#">Dynamips</a>
+      <a href="#">VMWare</a>
+      <a href="#">Docker</a>
+   </div>
+</div>
+
+<? include '../footer.php' ?>
---
+++ welcome/index.php
@@ -0,0 +1,65 @@
+<div class="welcome-splash">
+   <div id="welcome-particles"></div>
+   <div class="headline">
+      <div class="welcome-icon icon ion-ios-book-outline"></div>
+      <div class="title">Documentation</div>
+      <div class="subtitle">The official guide and reference for GNS3</div>
+      <div class="search-input">
+         <div class="icon ion-android-search"></div>
+         <input type="text" placeholder="Search Documentation">
+      </div>
+   </div>
+</div>
+<div class="welcome-container">
+   <div class="welcome-wrapper">
+      <div class="welcome-title">
+         <div class="title">Explore Documentation</div>
+         <div class="date">Last Updated Dec 1, 2016</div>
+      </div>
+      <a href="/explore" class="welcome-tile">
+         <span class="icon ion-folder green"></span>
+         <span class="label">Explore Topics</span>
+      </a>
+
+      <a href="/release-notes" class="welcome-tile">
+         <span class="icon ion-document-text green"></span>
+         <span class="label">Releases notes</span>
+      </a>
+
+      <a class="welcome-tile">
+         <span class="icon ion-social-windows"></span>
+         <span class="label">Getting started<br/>Windows</span>
+      </a>
+      <a class="welcome-tile">
+         <span class="icon ion-social-apple"></span>
+         <span class="label">Getting started<br/>Mac OSX</span>
+      </a>
+      <a class="welcome-tile">
+         <span class="icon ion-social-tux"></span>
+         <span class="label">Getting started<br/>Linux</span>
+      </a>
+      <a class="welcome-tile">
+         <span class="icon ion-wrench blue"></span>
+         <span class="label">Troubleshoot<br/>GNS3</span>
+      </a>
+      <a class="welcome-tile">
+         <span class="icon ion-arrow-down-a blue"></span>
+         <span class="label">Download</span>
+      </a>
+      <a class="welcome-tile">
+         <span class="icon ion-help purple"></span>
+         <span class="label">Support</span>
+      </a>
+      <a class="welcome-tile">
+         <span class="icon ion-university purple"></span>
+         <span class="label">Training</span>
+      </a>
+   </div>
+</div>
+<script type="text/javascript">
+   $(function(){
+      $.getJSON('/welcome/particles.json', function( data ) {
+         particlesJS('welcome-particles', data)
+      })
+   })
+</script>
